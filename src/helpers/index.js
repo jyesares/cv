ece89792dce109b1ca0669e07fbe391d49f80055
jyesares <javiyesares@gmail.com>
2018-05-03 03:40:19 +0200

    :bug: fix mobile alignment

--- src/helpers/index.js
+++
@@ -1,8 +0,0 @@
-import { css } from "emotion";
-import facepaint from "facepaint";
-
-const mq = facepaint([
-  "@media(min-width: 420px)",
-  "@media(min-width: 920px)",
-  "@media(min-width: 1120px)"
-]);
